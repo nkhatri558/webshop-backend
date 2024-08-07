0ae99c57fb16a1bb493051b5317c25b6b33bc76e
muhammad shaharyar <tallatammar3324@gmail.com>
2024-08-07 17:26:31 +0200

    Customer API Update

--- src/main/java/com/webshop/service/CustomerService.java
+++ src/main/java/com/webshop/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.webshop.service;
 
+import com.webshop.dto.CustomerDTO;
 import com.webshop.model.Customer;
 import com.webshop.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,23 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
-    public Optional<Customer> getCustomerById(Long id) {
-        return customerRepository.findById(id);
+    public Optional<CustomerDTO> getCustomerById(Long id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        return customer.map(this::convertToDTO);
+    }
+
+    private CustomerDTO convertToDTO(Customer customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setId(customer.getId());
+        customerDTO.setFirstName(customer.getFirstName());
+        customerDTO.setLastName(customer.getLastName());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setPhone(customer.getPhone());
+        customerDTO.setAddress1(customer.getAddress1());
+        customerDTO.setAddress2(customer.getAddress2());
+        customerDTO.setCity(customer.getCity());
+        customerDTO.setPostalCode(customer.getPostalCode());
+        return customerDTO;
     }
 
     public Customer getCustomerByEmail(String email) {
